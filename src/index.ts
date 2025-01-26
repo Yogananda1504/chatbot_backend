ef3649b6ffcb708416e4cf1a121db3cfb24d8137
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 13:22:02 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['Set-Cookie']
+
 }));
 app.use(express.json());
 app.use(cookieParser());
