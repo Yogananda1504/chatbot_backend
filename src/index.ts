823e01d471cd5cb0bc36754ec77c9a8f63d712c3
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 13:26:42 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,10 @@ connectToDB();
 const frontendUrl  = process.env.CLIENT_URL;
 console.log(frontendUrl)
 app.use(cors({
-    origin: "https://chatbot-vynr.netlify.app",
+    origin: frontendUrl,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-
 }));
 app.use(express.json());
 app.use(cookieParser());
