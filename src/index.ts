beb0d6db39a9c24bdf6bc95c0cfb5c03ddb18f04
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 22:00:34 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,9 @@ const PORT = 3000;
 connectToDB();
 const frontendUrl  = process.env.CLIENT_URL;
 console.log(frontendUrl)
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chatbot-vynr.netlify.app'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-    next();
-  });
-
+app.use(cors({
+    origin: frontendUrl,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
