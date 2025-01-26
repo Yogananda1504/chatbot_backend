169cadf57936b1a17f3ac92e3d82f77c35a06fdc
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 12:40:34 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ console.log(frontendUrl)
 app.use(cors({
     origin: `${frontendUrl}`, 
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
