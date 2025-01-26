e64b2469e2978c10d400c280446cad7d4e82fe86
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 22:44:35 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,12 @@ connectToDB();
 const frontendUrl  = process.env.CLIENT_URL;
 console.log(frontendUrl)
 app.use(cors({
-    origin: '*'
-  
+    origin: frontendUrl,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
