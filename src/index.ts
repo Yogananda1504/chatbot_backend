bb36e79a3197ef75b80eaff74c7684a96947eee0
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 22:33:11 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,11 @@ const PORT = 3000;
 connectToDB();
 const frontendUrl  = process.env.CLIENT_URL;
 console.log(frontendUrl)
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
