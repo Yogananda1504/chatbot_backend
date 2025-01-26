5229b83057453097cce298dc1c6fe01e686cb604
Yogananda1504 <vynr1504@gmail.com>
2025-01-26 22:34:28 +0530

    #

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,8 @@ connectToDB();
 const frontendUrl  = process.env.CLIENT_URL;
 console.log(frontendUrl)
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: '*'
+  
 }));
 app.use(express.json());
 app.use(cookieParser());
